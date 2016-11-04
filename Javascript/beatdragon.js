3aa19140a0aa5920ccdb9ac1e13fabcb9106cc66
Crystal Lu <syyssie@gmail.com>
2016-11-04 11:49:23 -0700

    Update beatdragon.js

--- Javascript/beatdragon.js
+++ Javascript/beatdragon.js
@@ -1,1 +1,27 @@
+var slaying = true;
+
+var youHit = (Math.floor(Math.random() * 2));
+
+var damageThisRound = (Math.floor(Math.random() * 5 + 1));
+
+var totalDamage = 0;
+
+while (slaying) {
+    if (youHit) {
+        console.log ("You hit the dragon by " + damageThisRound + " point!")
+        totalDamage += damageThisRound;
+
+        if (totalDamage >= 4) {
+            console.log ("You slew the dragon!")
+            slaying = false;
+        } else {
+            youHit = (Math.floor(Math.random() * 2));
+        }
+
+    } else {
+        console.log ("You lose!")
+        slaying = false;
+    }
+
+}
 
